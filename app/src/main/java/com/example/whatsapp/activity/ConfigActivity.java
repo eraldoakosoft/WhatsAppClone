0b1c522cc7c308e5459eb3e8e486648befbcadd9
eraldoakosoft <eraldo450518@outlook.com>
2020-03-29 17:26:35 -0300

    Segundo Commit

--- app/src/main/java/com/example/whatsapp/activity/ConfigActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/ConfigActivity.java
@@ -1,6 +1,7 @@
 package com.example.whatsapp.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,6 +10,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -16,6 +21,8 @@ import android.widget.ImageButton;
 
 import com.example.whatsapp.R;
 import com.example.whatsapp.helper.Permissao;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ConfigActivity extends AppCompatActivity {
 
@@ -27,6 +34,7 @@ public class ConfigActivity extends AppCompatActivity {
     private ImageButton imageButtonCamera, imageButtonGaleria;
     private static final int SELECAO_CAMERA = 100;
     private static final int SELECAO_GALERIA = 200;
+    private CircleImageView circleImageView;
 
 
     @Override
@@ -40,6 +48,7 @@ public class ConfigActivity extends AppCompatActivity {
 
         imageButtonCamera = findViewById(R.id.imageButtonConfigCamera);
         imageButtonGaleria = findViewById(R.id.imageButtonConfigGaleria);
+        circleImageView = findViewById(R.id.circleImageFotoPerfil);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
@@ -59,6 +68,60 @@ public class ConfigActivity extends AppCompatActivity {
         });
 
 
+
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i , SELECAO_GALERIA);
+                }
+            }
+        });
+
+
+    }
+
+    private Bitmap abreFotoERotaciona(String caminhoFoto, int angulo) {
+        // Abre o bitmap a partir do caminho da foto Bitmap
+        Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+
+        // Prepara a operação de rotação com o ângulo escolhido
+        Matrix matrix = new Matrix(); matrix.postRotate(angulo);
+
+        // Cria um novo bitmap a partir do original já com a rotação aplicada
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ( resultCode == RESULT_OK ){
+            Bitmap imagem = null;
+
+            try {
+                switch ( requestCode ){
+                    case SELECAO_CAMERA :
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA :
+                        Uri localImagensSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagensSelecionada);
+                        Matrix matrix = new Matrix();
+                        matrix.postRotate(-90);
+                        Bitmap rotated = Bitmap.createBitmap(imagem,0,0,imagem.getWidth(),imagem.getHeight(), matrix, true);
+                        imagem = rotated;
+                        break;
+                }
+                if (imagem != null){
+                    circleImageView.setImageBitmap( imagem );
+                }
+
+            }catch ( Exception e ){
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
